c490eb73fe50bf939670e91b057a1a7a3c1940fc
Raul <rchfe23@gmail.com>
2020-11-14 02:06:12 -0500

    lab1: add prints

--- labs/lab1_home_prices/lab1.py
+++ labs/lab1_home_prices/lab1.py
@@ -1,15 +1,22 @@
 # Ref.: Kaggle - Intro to ML
 import os
 import pandas as pd
+import ipdb
+
+
+def print_(msg):
+    print("*** {} ***".format(msg))
 
 
 # Load Melbourne Housing Snapshot dataset
 melbourne_file_path = os.path.expanduser('~/Data/kaggle_datasets/melbourne_housing_snapshot/melb_data.csv')
 melbourne_data = pd.read_csv(melbourne_file_path)
 # Print a summary of the data in Melbourne data
+print_("Summary of dataset")
 print(melbourne_data.describe(), end="\n\n")
 
 # List of all columns in the dataset
+print_("Columns")
 print(melbourne_data.columns, end="\n\n")
 
 # Drop missing values
@@ -17,9 +24,16 @@ melbourne_data = melbourne_data.dropna(axis=0)
 
 # Select the prediction target (price)
 y = melbourne_data.Price
+print_("y")
 print(y, end="\n\n")
 
 # Select features
 melbourne_features = ['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude']
 X = melbourne_data[melbourne_features]
+print_("X")
 print(X, end="\n\n")
+
+print_("Summary of X")
+print(X.describe(), end="\n\n")
+print_("First few rows of X")
+print(X.head(), end="\n\n")
