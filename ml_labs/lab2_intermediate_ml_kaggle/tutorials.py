fc8f99548028c946286c45e14235b08e85f620f7
Raul <rchfe23@gmail.com>
2020-11-15 11:55:01 -0500

    Update README

--- ml_labs/lab2_intermediate_ml_kaggle/tutorials.py
+++ ml_labs/lab2_intermediate_ml_kaggle/tutorials.py
@@ -5,13 +5,16 @@ https://www.kaggle.com/glovepm/melbourne-housing
 """
 import os
 
+import ipdb
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.impute import SimpleImputer
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
 from ml_labs.utils.genutils import print_
+from exercises import score_dataset
 
 melbourne_file_path = os.path.expanduser('~/Data/kaggle_datasets/melbourne_housing_snapshot/melb_data.csv')
 
@@ -23,20 +26,62 @@ def score_model(model, X_t, X_v, y_t, y_v):
     return mean_absolute_error(y_v, preds)
 
 
+def load_data():
+    # Read the data
+    data = pd.read_csv(melbourne_file_path)
+
+    # Separate target from predictors
+    y = data.Price
+    X = data.drop(['Price'], axis=1)
+
+    # Divide data into training and validation subsets
+    X_train_full, X_valid_full, y_train, y_valid = train_test_split(X, y, train_size=0.8, test_size=0.2,
+                                                                    random_state=0)
+
+    # Drop columns with missing values (simplest approach)
+    cols_with_missing = [col for col in X_train_full.columns if X_train_full[col].isnull().any()]
+    X_train_full.drop(cols_with_missing, axis=1, inplace=True)
+    X_valid_full.drop(cols_with_missing, axis=1, inplace=True)
+
+    # "Cardinality" means the number of unique values in a column
+    # Select categorical columns with relatively low cardinality (convenient but arbitrary)
+    low_cardinality_cols = [cname for cname in X_train_full.columns if X_train_full[cname].nunique() < 10 and
+                            X_train_full[cname].dtype == "object"]
+
+    # Select numerical columns
+    numerical_cols = [cname for cname in X_train_full.columns if X_train_full[cname].dtype in ['int64', 'float64']]
+
+    # Keep selected columns only
+    my_cols = low_cardinality_cols + numerical_cols
+    X_train = X_train_full[my_cols].copy()
+    X_valid = X_valid_full[my_cols].copy()
+
+    return X_train, X_valid, y_train, y_valid
+
+
+def load_data_v2(features=None):
+    # Load data
+    melbourne_data = pd.read_csv(melbourne_file_path)
+    # Choose target and features
+    y = melbourne_data.Price
+    if features:
+        X = melbourne_data[features]
+    else:
+        X = melbourne_data
+    # Split data into training and validation data,
+    # X_train, X_valid, y_train, y_valid = train_test_split(X, y, random_state=0)
+    return train_test_split(X, y, random_state=0)
+
+
 # Lesson 2: Missing values
 def lesson_2():
     # ----------------------------------
     # Example: Melbourne Housing dataset
     # ----------------------------------
     # Load data
-    melbourne_data = pd.read_csv(melbourne_file_path)
-    # Choose target and features
-    y = melbourne_data.Price
-    melbourne_features = ['Rooms', 'Bathroom', 'Landsize', 'BuildingArea',
-                          'YearBuilt', 'Lattitude', 'Longtitude']
-    X = melbourne_data[melbourne_features]
-    # Split data into training and validation data,
-    X_train, X_valid, y_train, y_valid = train_test_split(X, y, random_state=0)
+    features = ['Rooms', 'Bathroom', 'Landsize', 'BuildingArea',
+                'YearBuilt', 'Lattitude', 'Longtitude']
+    X_train, X_valid, y_train, y_valid = load_data_v2(features)
 
     # Build a random forest model
     forest_model = RandomForestRegressor(random_state=1)
@@ -107,5 +152,80 @@ def lesson_2():
     print_(missing_val_count_by_column[missing_val_count_by_column > 0])
 
 
+# Lesson 3: Categorical Variables
+def lesson_3():
+    # Load data
+    features = ['Type', 'Method', 'Regionname', 'Rooms', 'Distance', 'Postcode',
+                'Bedroom2', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude',
+                'Propertycount']
+    X_train, X_valid, y_train, y_valid = load_data()
+
+    # -------
+    # Example
+    # -------
+    print_("First 5 rows of train data", 0)
+    print_(X_train.head())
+
+    # Get list of categorical variables
+    s = (X_train.dtypes == 'object')
+    object_cols = list(s[s].index)
+
+    print_("Categorical variables:", 0)
+    print_(object_cols)
+
+    # --------------------------------------------------
+    # Score from Approach 1 (Drop Categorical Variables)
+    # --------------------------------------------------
+    drop_X_train = X_train.select_dtypes(exclude=['object'])
+    drop_X_valid = X_valid.select_dtypes(exclude=['object'])
+
+    print_("MAE from Approach 1 (Drop categorical variables):", 0)
+    print_(score_dataset(drop_X_train, drop_X_valid, y_train, y_valid))
+
+    # --------------------------------------
+    # Score from Approach 2 (Label Encoding)
+    # --------------------------------------
+    # Make copy to avoid changing original data
+    label_X_train = X_train.copy()
+    label_X_valid = X_valid.copy()
+
+    # Apply label encoder to each column with categorical data
+    label_encoder = LabelEncoder()
+    for col in object_cols:
+        label_X_train[col] = label_encoder.fit_transform(X_train[col])
+        label_X_valid[col] = label_encoder.transform(X_valid[col])
+
+    print_("MAE from Approach 2 (Label Encoding):", 0)
+    print_(score_dataset(label_X_train, label_X_valid, y_train, y_valid))
+    # We can expect an additional boost in performance if we provide
+    # better-informed labels for all ordinal variables (instead of randomly
+    # assigning, for each column, each unique value to a different integer,
+    # like we did)
+
+    # ----------------------------------------
+    # Score from Approach 3 (One-Hot Encoding)
+    # ----------------------------------------
+    # Apply one-hot encoder to each column with categorical data
+    OH_encoder = OneHotEncoder(handle_unknown='ignore', sparse=False)
+    OH_cols_train = pd.DataFrame(OH_encoder.fit_transform(X_train[object_cols]))
+    OH_cols_valid = pd.DataFrame(OH_encoder.transform(X_valid[object_cols]))
+
+    # One-hot encoding removed index; put it back
+    OH_cols_train.index = X_train.index
+    OH_cols_valid.index = X_valid.index
+
+    # Remove categorical columns (will replace with one-hot encoding)
+    num_X_train = X_train.drop(object_cols, axis=1)
+    num_X_valid = X_valid.drop(object_cols, axis=1)
+
+    # Add one-hot encoded columns to numerical features
+    OH_X_train = pd.concat([num_X_train, OH_cols_train], axis=1)
+    OH_X_valid = pd.concat([num_X_valid, OH_cols_valid], axis=1)
+
+    print_("MAE from Approach 3 (One-Hot Encoding):", 0)
+    print_(score_dataset(OH_X_train, OH_X_valid, y_train, y_valid))
+
+
 if __name__ == '__main__':
-    lesson_2()
+    # lesson_2()
+    lesson_3()
