2e1bc9b96b816474d4851ff9cfe2ce470fcf86ca
Raul <rchfe23@gmail.com>
2020-11-15 14:22:37 -0500

    Update genutils: add two newlines when printing nondata

--- ml_labs/utils/genutils.py
+++ ml_labs/utils/genutils.py
@@ -1,5 +1,6 @@
-def print_(msg, is_data=1):
+def print_(msg, is_data=1, newlines_for_nondata=False):
     if is_data:
         print(msg, end="\n\n")
     else:
-        print("*** {} ***".format(msg))
+        newlines = "\n\n" if newlines_for_nondata else "\n"
+        print("*** {} ***".format(msg), end=newlines)
