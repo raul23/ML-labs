c0979c2b21b403ea6f50104cca5f5897352f9dd0
Raul <rchfe23@gmail.com>
2020-11-16 18:44:37 -0500

    Add Lab4's Lesson 2: Indexing, Selecting & Assigning

--- ml_labs/lab4_pandas_kaggle/tutorials.py
+++ ml_labs/lab4_pandas_kaggle/tutorials.py
@@ -5,6 +5,7 @@ https://www.kaggle.com/zynicide/wine-reviews
 """
 import os
 
+import ipdb
 import pandas as pd
 
 from ml_labs.utils.genutils import print_
@@ -65,5 +66,107 @@ def lesson_1():
     print_(wine_reviews.head())
 
 
+# Exercise 2: Indexing, Selecting & Assigning
+def lesson_2():
+    print_("Lesson 2: Indexing, Selecting & Assigning", 0, 1)
+    reviews = pd.read_csv(wine_file_path, index_col=0)
+    pd.set_option('max_rows', 5)
+
+    # ----------------
+    # Native accessors
+    # ----------------
+    print_("Country column from reviews", 0)
+    print_(reviews.country)  # also reviews['country']
+
+    print_("First country from the country Series", 0)
+    print_(reviews.country[0])
+
+    # ------------------
+    # Indexing in pandas
+    # ------------------
+    # pandas' own accessor operators: loc and iloc
+    #
+    # NOTE: loc and iloc are row-first, column-second
+    # This is the opposite of what we do in native Python, which is
+    # column-first, row-second.
+
+    # Index-based selection: iloc
+    # NOTE 1: iloc requires numeric indexers,
+    # NOTE 2: iloc indexes exclusively
+    # Select the first row of data in a DataFrame
+    print_("First row of data", 0)
+    print_(reviews.iloc[0])
+
+    print_("Get the first column from a DataFrame", 0)
+    print_(reviews.iloc[:, 0])
+
+    print_("Get the first 3 rows from the country column", 0)
+    print_(reviews.iloc[:3, 0])
+
+    print_("Get the 2nd and 3rd rows from the country column", 0)
+    print_(reviews.iloc[1:3, 0])
+
+    print_("Get the first 3 rows from the country column using a list", 0)
+    print_(reviews.iloc[[0, 1, 2], 0])
+
+    print_("Get the 5 last elements from the dataset", 0)
+    print_(reviews.iloc[-5:])
+
+    # Label-based selection: loc
+    # NOTE 1: loc works with string indexers,
+    # NOTE 2: loc, meanwhile, indexes inclusively
+    print_("Get the first entry in reviews (using loc)", 0)
+    print_(reviews.loc[0, 'country'])
+
+    print_("Get columns from the dataset using loc", 0)
+    print_(reviews.loc[:, ['taster_name', 'taster_twitter_handle', 'points']])
+
+    # ----------------------
+    # Manipulating the index
+    # ----------------------
+    print_("set_index to the title field", 0)
+    print_(reviews.set_index("title"))
+
+    # ---------------------
+    # Conditional selection
+    # ---------------------
+    print_("Check if each wine is Italian or not", 0)
+    print_(reviews.country == 'Italy')
+
+    print_("Get italian wined", 0)
+    print_(reviews.loc[reviews.country == 'Italy'])
+
+    # AND: &
+    print_("Get italian wines that are better than average", 0)
+    print_(reviews.loc[(reviews.country == 'Italy') & (reviews.points >= 90)])
+
+    # OR : | (pipe)
+    print_("Get italian or better than average wines", 0)
+    print_(reviews.loc[(reviews.country == 'Italy') | (reviews.points >= 90)])
+
+    # isin conditional selector
+    print_("Get wines from Italy or France", 0)
+    print_(reviews.loc[reviews.country.isin(['Italy', 'France'])])
+
+    # isnull and notnull: is (or not) empty (NaN)
+    print_("Get wines with a price tag", 0)
+    print_(reviews.loc[reviews.price.notnull()])
+
+    # --------------
+    # Assigning data
+    # --------------
+    # Assign a constant value
+    # Every row gets 'everyone'
+    reviews['critic'] = 'everyone'
+    print_("Assign a constant value", 0)
+    print_(reviews['critic'])
+
+    # Assign an iterable of values
+    reviews['index_backwards'] = range(len(reviews), 0, -1)
+    print_("Assign an iterable of values", 0)
+    print_(reviews['index_backwards'])
+
+
 if __name__ == '__main__':
-    lesson_1()
+    # lesson_1()
+    lesson_2()
