005c6c838907779f8cb2accfb2a1afb6e33c777d
Raul <rchfe23@gmail.com>
2020-11-16 13:46:04 -0500

    Add Lab3's Ex3: Feature Generation

--- ml_labs/lab3_feature_engineering_kaggle/exercises.py
+++ ml_labs/lab3_feature_engineering_kaggle/exercises.py
@@ -3,6 +3,7 @@
 The TalkingData AdTracking dataset can be downloaded from
 https://www.kaggle.com/c/talkingdata-adtracking-fraud-detection
 """
+import itertools
 import os
 
 import ipdb
@@ -11,6 +12,7 @@ import lightgbm as lgb
 import pandas as pd
 from sklearn import metrics
 from sklearn import preprocessing
+from sklearn.preprocessing import LabelEncoder
 
 from ml_labs.utils.genutils import print_
 
@@ -150,9 +152,9 @@ def ex_2():
     _ = train_model(train, valid)
     print()
 
-    # ---------------------------------
+    # ------------------------------------
     # 1. Categorical encodings and leakage
-    # ---------------------------------
+    # ------------------------------------
 
     # ------------------
     # 2. Count encodings
@@ -220,6 +222,122 @@ def ex_2():
     print()
 
 
+# Exercise 3: Feature Generation
+def ex_3():
+    print_("Exercise 3: Feature Generation", 0, 1)
+    # -----
+    # Setup
+    # -----
+    # Create features from   timestamps
+    click_data = pd.read_csv(adtracking_file_path,
+                             parse_dates=['click_time'])
+    click_times = click_data['click_time']
+    clicks = click_data.assign(day=click_times.dt.day.astype('uint8'),
+                               hour=click_times.dt.hour.astype('uint8'),
+                               minute=click_times.dt.minute.astype('uint8'),
+                               second=click_times.dt.second.astype('uint8'))
+
+    # Label encoding for categorical features
+    cat_features = ['ip', 'app', 'device', 'os', 'channel']
+    for feature in cat_features:
+        label_encoder = preprocessing.LabelEncoder()
+        clicks[feature] = label_encoder.fit_transform(clicks[feature])
+
+    def get_data_splits(dataframe, valid_fraction=0.1):
+
+        dataframe = dataframe.sort_values('click_time')
+        valid_rows = int(len(dataframe) * valid_fraction)
+        train = dataframe[:-valid_rows * 2]
+        # valid size == test size, last two sections of the data
+        valid = dataframe[-valid_rows * 2:-valid_rows]
+        test = dataframe[-valid_rows:]
+
+        return train, valid, test
+
+    def train_model(train, valid, test=None, feature_cols=None):
+        if feature_cols is None:
+            feature_cols = train.columns.drop(['click_time', 'attributed_time',
+                                               'is_attributed'])
+        dtrain = lgb.Dataset(train[feature_cols], label=train['is_attributed'])
+        dvalid = lgb.Dataset(valid[feature_cols], label=valid['is_attributed'])
+
+        param = {'num_leaves': 64, 'objective': 'binary',
+                 'metric': 'auc', 'seed': 7, 'verbose': -1}
+        num_round = 1000
+        # print("Training model. Hold on a minute to see the validation score")
+        bst = lgb.train(param, dtrain, num_round, valid_sets=[dvalid],
+                        early_stopping_rounds=20, verbose_eval=False)
+
+        valid_pred = bst.predict(valid[feature_cols])
+        valid_score = metrics.roc_auc_score(valid['is_attributed'], valid_pred)
+        print(f"Validation AUC score: {valid_score}")
+
+        if test is not None:
+            test_pred = bst.predict(test[feature_cols])
+            test_score = metrics.roc_auc_score(test['is_attributed'], test_pred)
+            return bst, valid_score, test_score
+        else:
+            return bst, valid_score
+
+    print_("Baseline model score", 0)
+    train, valid, test = get_data_splits(clicks)
+    _ = train_model(train, valid)
+    print()
+
+    # ---------------------------
+    # 1. Add interaction features
+    # ---------------------------
+    cat_features = ['ip', 'app', 'device', 'os', 'channel']
+    interactions = pd.DataFrame(index=clicks.index)
+
+    # Iterate through each pair of features, combine them into interaction features
+    encoder = LabelEncoder()
+    for col1, col2 in itertools.combinations(cat_features, 2):
+        new_col_name = '_'.join([col1, col2])
+
+        # Convert to strings and combine
+        new_values = clicks[col1].map(str) + "_" + clicks[col2].map(str)
+
+        interactions[new_col_name] = encoder.fit_transform(new_values)
+
+    clicks = clicks.join(interactions)
+    print_("Score with interactions", 0)
+    train, valid, test = get_data_splits(clicks)
+    _ = train_model(train, valid)
+    print()
+
+    # Generating numerical features
+
+    # -----------------------------------------
+    # 2. Number of events in the past six hours
+    # -----------------------------------------
+    def count_past_events(series):
+        series = pd.Series(series.index, index=series)
+        past_events = series.rolling('6h').count() - 1
+        return past_events
+
+    ipdb.set_trace()
+
+    # -----------------------------------
+    # 3. Features from future information
+    # -----------------------------------
+
+    # ------------------------
+    # 4. Time since last event
+    # ------------------------
+    def time_diff(series):
+        """Returns a series with the time since the last timestamp in seconds."""
+        return series.diff().dt.total_seconds()
+
+    # -----------------------------------
+    # 5. Number of previous app downloads
+    # -----------------------------------
+    def previous_attributions(series):
+        """Returns a series with the number of times an app has been downloaded."""
+        return series.expanding(min_periods=2).sum() - series
+
+
 if __name__ == '__main__':
     # ex_1()
-    ex_2()
+    # ex_2()
+    ex_3()
