372d011b5a30f135eeec9a6266c89a26e9ac4b6c
Raul <rchfe23@gmail.com>
2020-11-15 22:06:55 -0500

    Add Lab2's Ex6

--- ml_labs/lab2_intermediate_ml_kaggle/exercises.py
+++ ml_labs/lab2_intermediate_ml_kaggle/exercises.py
@@ -16,6 +16,7 @@ from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import cross_val_score, train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from xgboost import XGBRegressor
 
 from ml_labs.utils.genutils import print_
 
@@ -73,6 +74,44 @@ def load_data_for_ex_5():
     X_test = test_data[numeric_cols].copy()
 
     return X, y, X_test
+
+
+def load_data_for_ex_6():
+    # Read the data
+    X = pd.read_csv(train_file_path, index_col='Id')
+    X_test_full = pd.read_csv(test_file_path, index_col='Id')
+
+    # Remove rows with missing target, separate target from predictors
+    X.dropna(axis=0, subset=['SalePrice'], inplace=True)
+    y = X.SalePrice
+    X.drop(['SalePrice'], axis=1, inplace=True)
+
+    # Break off validation set from training data
+    X_train_full, X_valid_full, y_train, y_valid = train_test_split(X, y, train_size=0.8, test_size=0.2,
+                                                                    random_state=0)
+
+    # "Cardinality" means the number of unique values in a column
+    # Select categorical columns with relatively low cardinality (convenient but arbitrary)
+    low_cardinality_cols = [cname for cname in X_train_full.columns if X_train_full[cname].nunique() < 10 and
+                            X_train_full[cname].dtype == "object"]
+
+    # Select numeric columns
+    numeric_cols = [cname for cname in X_train_full.columns if X_train_full[cname].dtype in ['int64', 'float64']]
+
+    # Keep selected columns only
+    my_cols = low_cardinality_cols + numeric_cols
+    X_train = X_train_full[my_cols].copy()
+    X_valid = X_valid_full[my_cols].copy()
+    X_test = X_test_full[my_cols].copy()
+
+    # One-hot encode the data (to shorten the code, we use pandas)
+    X_train = pd.get_dummies(X_train)
+    X_valid = pd.get_dummies(X_valid)
+    X_test = pd.get_dummies(X_test)
+    X_train, X_valid = X_train.align(X_valid, join='left', axis=1)
+    X_train, X_test = X_train.align(X_test, join='left', axis=1)
+
+    return X_train, X_valid, y_train, y_valid, X_test
 
 
 # Function for comparing different approaches
@@ -513,10 +552,67 @@ def ex_5():
     # -------------------------------------
 
 
+# Exercise 6: XGBoost
+def ex_6():
+    print_("Exercise 6: XGBoost", 0, 1)
+    X_train, X_valid, y_train, y_valid, X_test = load_data_for_ex_6()
+    # -------------------
+    # Step 1: Build model
+    # -------------------
+    # Part A
+    # Define the model
+    my_model_1 = XGBRegressor(random_state=0)
+
+    # Fit the model
+    my_model_1.fit(X_train, y_train)
+
+    # Part B
+    # Get predictions
+    predictions_1 = my_model_1.predict(X_valid)
+
+    # Part C
+    # Calculate MAE
+    mae_1 = mean_absolute_error(predictions_1, y_valid)
+    print("Mean Absolute Error:", mae_1)
+
+    # -------------------------
+    # Step 2: Improve the model
+    # -------------------------
+    my_model_2 = XGBRegressor(random_state=0, n_estimators=1000, learning_rate=0.05, n_jobs=4)
+
+    # Fit the model
+    my_model_2.fit(X_train, y_train,
+                   early_stopping_rounds=5,
+                   eval_set=[(X_valid, y_valid)],
+                   verbose=False)
+
+    # Get predictions
+    predictions_2 = my_model_2.predict(X_valid)
+
+    # Calculate MAE
+    mae_2 = mean_absolute_error(predictions_2, y_valid)
+    print("Mean Absolute Error:" , mae_2)
+
+    # -----------------------
+    # Step 3: Break the model
+    # -----------------------
+    my_model_3 = XGBRegressor(random_state=0, n_estimators=10, learning_rate=0.9)
+
+    # Fit the model
+    my_model_3.fit(X_train, y_train)
+
+    # Get predictions
+    predictions_3 = my_model_3.predict(X_valid)
+
+    # Calculate MAE
+    mae_3 = mean_absolute_error(predictions_3, y_valid)
+    print("Mean Absolute Error:", mae_3)
+
 
 if __name__ == '__main__':
     # ex_1()
     # ex_2()
     # ex_3()
     # ex_4()
-    ex_5()
+    # ex_5()
+    ex_6()
