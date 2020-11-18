de922aa0200b59cdb7129499e7a0cba1e7d52f58
Raul <rchfe23@gmail.com>
2020-11-18 15:50:12 -0500

    Add Lab5 - Exercise 3: Bar Charts and Heatmaps

--- ml_labs/lab5_data_visualization_kaggle/exercises.py
+++ ml_labs/lab5_data_visualization_kaggle/exercises.py
@@ -14,6 +14,7 @@ from ml_labs.utils.genutils import print_
 
 fifa_filepath = os.path.expanduser('~/Data/kaggle_datasets/fifa_kaggle_course/fifa.csv')
 museum_filepath = os.path.expanduser('~/Data/kaggle_datasets/museum_visitors_kaggle_course/museum_visitors.csv')
+ign_filepath = os.path.expanduser('~/Data/kaggle_datasets/ign_scores_kaggle_course/ign_scores.csv')
 
 
 # Exercise 1: Hello, Seaborn
@@ -99,10 +100,75 @@ def ex_2():
     plt.show()
 
 
+# Exercise 3: Bar Charts and Heatmaps
+def ex_3():
+    print_("Exercise 3: Bar Charts and Heatmaps", 0, 1)
+    # ---------------------
+    # Step 1: Load the data
+    # ---------------------
+    ign_data = pd.read_csv(ign_filepath, index_col="Platform")
+
+    # -----------------------
+    # Step 2: Review the data
+    # -----------------------
+    # Print the data
+    print_("The whole data", 0)
+    print_(ign_data)
+
+    # What is the highest average score received by PC games, for any platform?
+    high_score = 7.759930
+
+    # On the Playstation Vita platform, which genre has the lowest average score?
+    # Please provide the name of the column, and put your answer in single quotes
+    # (e.g., 'Action', 'Adventure', 'Fighting', etc.)
+    worst_genre = 'Simulation'
+
+    # -------------------------------
+    # Step 3: Which platform is best?
+    # -------------------------------
+    # Part A
+    # Create a bar chart that shows the average score for racing games, for each
+    # platform. Your chart should have one bar for each platform.
+
+    # Bar chart showing average score for racing games by platform
+    # Set the width and height of the figure
+    plt.figure(figsize=(10, 6))
+
+    # Add title
+    plt.title("Average score for racing games by platform")
+
+    # Bar chart showing average score for racing games by platform
+    sns.barplot(x=ign_data.index, y=ign_data['Racing'])
+
+    # Add label for vertical axis
+    plt.ylabel("Average score")
+
+    plt.show()
+
+    # ----------------------------------
+    # Step 4: All possible combinations!
+    # ----------------------------------
+    # Part A
+    # Heatmap showing average game score by platform and genre
+    # Set the width and height of the figure
+    plt.figure(figsize=(14, 7))
+
+    # Add title
+    plt.title("Average game score by platform and genre")
+
+    # Heatmap showingaverage game score by platform and genre
+    sns.heatmap(data=ign_data, annot=True)
+
+    # Add label for horizontal axis
+    plt.xlabel("Platform")
+
+    plt.show()
+
+
 if __name__ == '__main__':
     # ex_1()
-    ex_2()
-    # ex_3()
+    # ex_2()
+    ex_3()
     # ex_4()
     # ex_5()
     # ex_6()
